cc78f5162b33af721dbc7319bda0639da9c389a8
Rafael <rafaelrehfeld97@gmail.com>
2023-04-03 18:36:00 -0300

    add register user

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connect(){
   
-  mongoose.connect('mongodb://127.0.0.1:27017/meubanco')
+  mongoose.connect('mongodb://127.0.0.1:27017/projeto-crud')
   
   const db = mongoose.connection
   
