d3fc9c5d0283419b9a2e67a37df47aa474b1a49f
Rafael <rafaelrehfeld97@gmail.com>
2023-04-04 20:20:35 -0300

    add responsive design

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,11 @@
-console.log('Ok, funcionando!')
+const buttonMenuAbrir = document.querySelector('.buttonMenu')
+const menuMobile = document.querySelector('.menu-mobile')
+const buttonDelete = document.querySelector('.buttonDelete')
+
+buttonMenuAbrir.addEventListener('click', ()=>{
+  menuMobile.classList.add('visibility')
+})
+
+buttonDelete.addEventListener('click', ()=>{
+  menuMobile.classList.remove('visibility')
+})
